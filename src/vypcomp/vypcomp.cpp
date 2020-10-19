060e1da585007b39223ac0029278900fd1944215
Peter Kubov <peter.kubov@avast.com>
2020-10-19 20:25:19 +0200

    catch exceptions

--- src/vypcomp/vypcomp.cpp
+++ src/vypcomp/vypcomp.cpp
@@ -6,8 +6,22 @@ using namespace vypcomp;
 
 int main(int argc, char *argv[])
 {
-	LangParser parser;
-	parser.parse(std::cin);
+	try {
+		LangParser parser;
+		parser.parse(std::cin);
+	}
+	catch (const LexicalError& le) {
+		std::cerr << "lexical error: " << le.what() << std::endl;
+		return 11;
+	}
+	catch (const ParserError& pe) {
+		std::cerr << "syntax error: " << pe.what() << std::endl;
+		return 12;
+	}
+	catch (const std::exception& e) {
+		std::cerr << "error: " << e.what() << std::endl;
+		return 19;
+	}
 
 	return 0;
 }
