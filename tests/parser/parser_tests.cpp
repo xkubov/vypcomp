ebe3af3c5a9917faadf2404fac4ca12b62526f34
Peter Kubov <peter.kubov@avast.com>
2020-10-19 19:38:07 +0200

    tests/parser: provide initial tests

--- tests/parser/parser_tests.cpp
+++ tests/parser/parser_tests.cpp
@@ -1,1 +1,28 @@
+#include <gtest/gtest.h>
+
+#include <sstream>
+
 #include "vypcomp/parser/parser.h"
+
+using namespace ::testing;
+
+using namespace vypcomp;
+
+class ParserTests : public Test {};
+
+TEST_F(ParserTests, emptyInput)
+{
+	std::stringstream input;
+	std::ostringstream output;
+
+	LangParser parser;
+	ASSERT_NO_THROW(parser.parse(input));
+	ASSERT_NO_THROW(parser.generateOutput(output));
+	ASSERT_TRUE(output.str().empty());
+}
+
+TEST_F(ParserTests, invalidFile)
+{
+	LangParser parser;
+	ASSERT_THROW(parser.parse("pls_dont_create_file_with_this_name"), std::runtime_error);
+}
